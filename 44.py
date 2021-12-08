56eed7f5d7f0e4a36b5adba7439b2be0912b7cbb
ErrorID10T <brian.jeffrey.good@gmail.com>
2021-12-07 19:17:48 -0700

    Committed!

---
+++ 44.py
@@ -0,0 +1,17 @@
+import time
+from types import TracebackType
+
+start_time = time.time()
+
+def is_pentagonal(n):
+    if (1+24*n+1)**0.5) % 6 == 0
+        return True
+    return False
+
+found = False
+
+while not found:
+    for 
+
+
+print(time.time() - start_time)
---
+++ add_zeroes.py
@@ -0,0 +1,5 @@
+from os import listdir
+files = listdir()
+for file in files:
+    print(file)
+    
