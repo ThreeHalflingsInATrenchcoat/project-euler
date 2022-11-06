55c0700273b0db0d14a0d00f2eb7fa360d687a73
Brian Good <brian.jeffrey.good@gmail.com>
2022-11-06 13:20:38 -0700

    new record, 15% difficulty, I feel like that is somehow not a compliment

--- 049.py
+++ 049.py
@@ -1,12 +1,19 @@
-# The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways: (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
+# The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways:
+# (i) each of the three terms are prime, and, 
+# (ii) each of the 4-digit numbers are permutations of one another.
 # There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there is one other 4-digit increasing sequence.
 # What 12-digit number do you form by concatenating the three terms in this sequence?
+
+import tools.timer as timer
 import itertools
 import tools.numbers as numbers
 
 # boundaries for problem
 lower = 1000
 upper = 9999
+
+t = timer.Timer(verbose=True)
+t.start()
 
 #generate initial prime, -1 because READ THE FUNCTION DESCRIPTION
 primes = [numbers.next_prime(lower - 1)]
@@ -19,23 +26,38 @@ primes.pop()
 
 primes_groups = []
 
-#convert primes to 
-for prime in primes:
-    #convert `prime` int to str (iterable), create permutations(list of tuples) 
+while len(primes) > 0:    
+    #grab a prime
+    prime = primes[-1]
+    #create prime permutations
     prime_perms = list(itertools.permutations(str(prime)))
+    #convert permutations back to ints
     for i in range(len(prime_perms)):
-        #convert tuples to strings, then back to ints... could be done through math, but this was faster to type and what I thought of first
         prime_perms[i] = int(''.join(prime_perms[i]))
-    
-    primes_groups.append(prime_perms)
 
-#this was just to verify that the loop wasn't wonky
-print(len(primes))
-print(len(primes_groups))
+    primes_group = []
 
+    #find primes that match permutations and add to group
+    for prime_perm in prime_perms:
+        if prime_perm in primes:
+            primes_group.append(primes.pop(primes.index(prime_perm)))
 
-# NEXT TIME... ON PROJECT EULER
-# Continue the function from lines 23-28. Rather than making a list of all of the permutations of ALL of the primes,
-# iterate through the prime_perms lists after they are created, and remove any permutations that are NOT found in `primes`,
-# then remove any primes from `primes` that are listed in prime_perms
-# I'll be left with groups of primes that are permutations of each other
+    #add group only if it contains the minimum necessary number of primes
+    if len(prime_perms) >= 3:
+        primes_groups.append(primes_group)
+
+#sort groups for easy mathing
+for primes_group in primes_groups:
+    primes_group.sort()
+
+for primes_group in primes_groups:
+    for i in range(len(primes_group)-2):
+        first = primes_group[i]
+        for j in range(i + 1, len(primes_group)-1):
+            second = primes_group[j]
+            diff = second - first
+            third = second + diff
+            if third in primes_group:
+                print(first, second, third, sep=', ')
+
+t.stop()
